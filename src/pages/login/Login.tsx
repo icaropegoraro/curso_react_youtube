470255547869021bb264647d972b0b2fb61d8dce
Ícaro Pegoraro <icaropegsakurai@gmail.com>
2025-06-16 21:03:01 -0300

    Fazendo um formulário simples utilizando useState()

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,14 +1,37 @@
-import { useNavigate } from "react-router-dom"
+import { useState } from "react"
 
 export const Login = () => {
-    const navigate = useNavigate()
-    const handleClick = () => {
-        navigate('/pagina-inicial')
+
+    const [email, setEmail] = useState('')
+    const [senha, setSenha] = useState('')
+
+    const handleEntrar = () => {
+        console.log(email)
+        console.log(senha)
     }
     return (
         <div>
-            <p>Login</p>
-            <button onClick={handleClick}>Testar</button>
+            <form action="">
+
+                <label>
+                    <span>Email</span>
+                    <input type='email' value={email} onChange={ evento => setEmail(evento.target.value)}/>
+                </label>
+
+                <br/>
+                <br/>
+
+                <label>
+                    <span>Senha</span>
+                    <input type='password' value={senha} onChange={ evento => setSenha(evento.target.value)}/>
+                </label>
+
+                <br />
+                <br />
+
+                <button type='button' onClick={handleEntrar}>Entrar</button>
+
+            </form>
         </div>
     )
 }
